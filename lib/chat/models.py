a20276940801e7c55c56171db6172db5fcc90699
Arturo Escudero <darkturo@gmail.com>
2017-05-29 17:45:00 +0200

    Add password support when registering
    Fix model to handle passwords.
    
    Update model so it stores the sha512 hash from the password

--- lib/chat/models.py
+++ lib/chat/models.py
@@ -1,15 +1,19 @@
 from sqlalchemy import Column, Integer, String
 from chat.database import Base
 
+from hashlib import sha512
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     email = Column(String(120), unique=True)
+    password = Column(String(16), unique=True)
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, email=None, password=None):
         self.name = name
         self.email = email
+        self.email = sha512.new(password).hexdigest()
 
     def __repr__(self):
         return '<User %r>' % (self.name)
